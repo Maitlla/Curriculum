2687abf0053745973ee00b99bb6193b052dbbc94
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 13:46:00 +0100

    ....

--- index.js
+++ index.js
@@ -11,8 +11,30 @@ function main() {
     for (let item of elem){
         item.addEventListener("pointerenter",()=>{console.log("POP!")})
         //console.log(item)
+
+
+
     }
     
+
+    const a = ()=>{const z = 1; const x = 2; return x}
+
+    const counters = elements.map(
+        (item) => {
+            const obj = {
+                element: item,
+                counter: 0,
+            }
+            item.addEventListener("pointerenter",()=>{
+                obj.counter++;
+                console.log(obj)
+
+            });
+            return obj
+        }
+     
+
+  
 
 
 }
