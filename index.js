9e8be8b23c6b2af9ca5d8aac00800c09a1be2abf
AFD Mañanas <afdm@postal6.academiapostal.es.local>
2022-02-11 12:45:56 +0100

    ...

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 function main() {
     console.log("Starting...");
-    const titulo = document.querySelector("h1");
-    titulo.addEventListener("pointerenter", imprimeHola);
+
+    //const titulo = document.querySelector("h1");
+    //titulo.addEventListener("pointerenter", imprimeHola);
+
+
+    const elements = Array.from(document.body.querySelectorAll("*"));
+    console.log(elements);
+    const elem = document.body.querySelectorAll("*")
+    for (let item of elem){
+        item.addEventListener("pointerenter",()=>{console.log("POP!")})
+        //console.log(item)
+    }
+    
+
+
 }
 
-function imprimeHola(){
-    console.log("Hola");
-}
+//function imprimeHola(){
+    //console.log("Hola");
+//}
 
 
 window.addEventListener("load",main);
