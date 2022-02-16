cea41e5dd236915f1939fe4880d3556a58ca7303
Maitlla <maitlla@gmail.com>
2022-02-17 00:47:15 +0100

    cambio-index.js

---
+++ src/js/fmain.js
@@ -0,0 +1,51 @@
+function main() {
+    console.log("Starting...");
+
+
+    }
+
+
+
+/*
+function main() {
+    console.log("Starting...");
+
+    //const titulo = document.querySelector("h1");
+    //titulo.addEventListener("pointerenter", imprimeHola);
+
+
+    const elements = Array.from(document.body.querySelectorAll("*"));
+    console.log(elements);
+    const elem = document.body.querySelectorAll("*")
+    for (let item of elem){
+        item.addEventListener("pointerenter",()=>{console.log("POP!")})
+        //console.log(item)
+
+    }
+    
+    const a = ()=>{const z = 1; const x = 2; return x}
+
+    const counters = elements.map(
+        (item) => {
+            const obj = {
+                element: item,
+                counter: 0,
+            }
+            item.addEventListener("pointerenter",()=>{
+                obj.counter++;
+                console.log(obj)
+
+            });
+            return obj
+        }
+
+}
+       
+//function imprimeHola(){
+    //console.log("Hola");
+//}
+
+*/
+
+window.addEventListener("load",main);
+
--- src/js/index.js
+++ src/js/index.js
@@ -1,51 +1,70 @@
 
-function main() {
-    console.log("Starting...");
+function app() {
+    const numbers = [22, 1, 4, 3, 5, 7, 8, 9, 12, 11];
+    const odd = []; //impar  si el módulo es 1
+    const even = [] //par    si el módulo es 0
+
+    for (let item of numbers) {
+        console.log("item:", item);
+
+        // let item = numbers[i];
+
+ 
+
+        // Put your code here
+
 
 
     }
+
+    console.log("odd:", odd);
+    console.log("even:", even);
+}
+
 
 
 
 /*
-function main() {
-    console.log("Starting...");
+console.log("Inicio");
 
-    //const titulo = document.querySelector("h1");
-    //titulo.addEventListener("pointerenter", imprimeHola);
+console.log("Operación de módulo:");
+console.log("13%10:", 13%10);
 
+console.log("Par/impar");
+console.log("33:", 33%2);
+console.log("32:", 32%2);
 
-    const elements = Array.from(document.body.querySelectorAll("*"));
-    console.log(elements);
-    const elem = document.body.querySelectorAll("*")
-    for (let item of elem){
-        item.addEventListener("pointerenter",()=>{console.log("POP!")})
-        //console.log(item)
+console.log("Añadir elementos a array");
+const number = 33;
+const numbers = [];
+numbers.push(number);
+numbers.push(41);
+console.log("numbers:", numbers);
 
-    }
-    
-    const a = ()=>{const z = 1; const x = 2; return x}
+console.log("Operadores condicionales:");
+console.log("33 > 34:", 33 > 34);
+console.log("33 < 34:", 33 < 34);
+console.log("33 > 33:", 33 > 33);
+console.log("33 >= 33:", 33 >= 33);
+console.log("33 === 33:", 33 === 33);
+console.log("'33' === 33:", "33" === 33);
 
-    const counters = elements.map(
-        (item) => {
-            const obj = {
-                element: item,
-                counter: 0,
-            }
-            item.addEventListener("pointerenter",()=>{
-                obj.counter++;
-                console.log(obj)
-
-            });
-            return obj
-        }
-
+console.log("Bifurcación lógica");
+const n1 = 44;
+const n2 = 55;
+if (n1 >= n2) {
+    console.log("n1-n2:", n1-n2)
 }
-       
-//function imprimeHola(){
-    //console.log("Hola");
-//}
+if (n1 < n2) {
+    console.log("n2-n1:", n2-n1)
+}
+
+if (n1 >= n2) {
+    console.log("n1-n2:", n1-n2)
+} else {
+    console.log("n2-n1:", n2-n1)
+}
 
 */
 
-window.addEventListener("load",main);
+
